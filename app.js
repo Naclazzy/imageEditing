efff597ec7513143306487c985d89fe20afc4cdf
JANWE <jan.wegmann@hotmail.de>
2022-07-29 23:43:13 +0200

    add prev image DOM Manipulator

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fileInput = document.querySelector(".file-input");
 const chooseImgBtn = document.querySelector(".choose-img"); 
-const showImage = document.querySelector(".preview-img img");                                                                                  
+const showImage = document.querySelector(".preview-img img");       
+const filterOptions = document.querySelectorAll(".filter button");                                                                                  
+
 
 const loadImage = () => {
     let file = fileInput.files[0];
@@ -12,5 +14,12 @@ const loadImage = () => {
     })
 }
 
+filterOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        document.querySelector(".filter .active").classList.remove("active")
+        option.classList.add("active")
+    })
+})
+
 fileInput.addEventListener("change", loadImage)
-chooseImgBtn.addEventListener("click", () => fileInput.click())
+chooseImgBtn.addEventListener("click", () => fileInput.click());
